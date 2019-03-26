db616ed408bb32e0137690167e097216d274a9d6
root <oscarmui1127@gmail.com>
2019-03-26 17:44:28 +0800

    partial progress in editing the contacts page, continue tmr

--- models/Enquiry.js
+++ models/Enquiry.js
@@ -14,16 +14,15 @@ var Enquiry = new keystone.List('Enquiry', {
 Enquiry.add({
 	name: { type: Types.Name, required: true },
 	email: { type: Types.Email, required: true },
-	phone: { type: String },
+	message: { type: Types.Markdown, required: true },
+	createdAt: { type: Date, default: Date.now }/*,
 	enquiryType: { type: Types.Select, options: [
 		{ value: 'message', label: 'Just leaving a message' },
 		{ value: 'question', label: 'I\'ve got a question' },
 		{ value: 'other', label: 'Something else...' },
-	] },
-	message: { type: Types.Markdown, required: true },
-	createdAt: { type: Date, default: Date.now },
+	] },*/
 });
 
 Enquiry.defaultSort = '-createdAt';
-Enquiry.defaultColumns = 'name, email, enquiryType, createdAt';
+Enquiry.defaultColumns = 'name, email, message|40%, createdAt';
 Enquiry.register();
